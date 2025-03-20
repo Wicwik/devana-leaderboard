5c5de11dae9ef4dde284d8f7bde9b7936221c90d
Robert <belanecrobert22@gmail.com>
2025-03-20 20:15:24 +0100

    get data script

--- telegraf/get_devana_data.sh
+++ telegraf/get_devana_data.sh
@@ -3,7 +3,13 @@
 projects=(p904-24-3)
 
 get_users() {
-	
+	ssh devana "sacctmgr -np show association where account=$1 | cut -f3 -d\| | awk 'NF'"
 }
 
-for project in $(proojects):
+for project in ${projects[@]};
+do
+    for user in `get_users $project`;
+    do
+        echo "checking: $user";
+    done
+done
