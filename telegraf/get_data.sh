5a59b5c22a11d04eddf69c6cad3e6cee2a1d854f
Robert Belanec <belanecrobert22@gmail.com>
2025-03-21 13:05:35 +0100

    updated telegraf.conf

--- telegraf/get_data.sh
+++ telegraf/get_data.sh
@@ -4,8 +4,12 @@
 # projects to check
 projects=(p904-24-3)
 
+get_kinit_usage_raw() {
+    scontrol -o show assoc_mgr | grep "Account=$1 UserName= Partition=" | awk '{print $8}' | awk -F/ '{print $3}' | sed -e 's/Efctv=//g'
+}
+
 get_users() {
-    sacctmgr -np show association where account=$1 | cut -f3 -d\| | awk 'NF'
+    sacctmgr -np show association where account=$1 | cut -f3 -d\| | awk NF
 }
 
 get_gpu_usage_raw() {
@@ -17,6 +21,12 @@ used_hour_sum=0
 
 for project in ${projects[@]};
 do
+    kinit_usage_raw=`get_kinit_usage_raw $project`;
+    kinit_usage_hour=`echo $kinit_usage_raw/57600 | bc`;
+    used_raw_sum=`echo $used_raw_sum + $kinit_usage_raw | bc`
+    used_hour_sum=`echo $used_hour_sum + $kinit_usage_hour | bc`
+
+    echo "gpu,project=$project,user=kinit used_raw=$kinit_usage_raw,used_hour=$kinit_usage_hour `date +%s%N`";
     for user in `get_users $project`;
     do
         gpu_usage_raw=`get_gpu_usage_raw $project $user`;
@@ -28,5 +38,4 @@ do
     done
 done
 
-echo $used_hour_sum
-echo $used_raw_sum
+echo "gpu,project=$project used_raw_total=$used_raw_total,used_hour_total=$used_hour_sum `date +%s%N`"
