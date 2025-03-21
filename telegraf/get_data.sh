fb2b83f70d6357ca31510ca582b14a48f05c8bed
Robert Belanec <belanecrobert22@gmail.com>
2025-03-21 12:16:38 +0100

    get_data from host

---
+++ telegraf/get_data.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# put this file in the home directory of data fetcher user
+
+# projects to check
+projects=(p904-24-3)
+
+get_users() {
+    sacctmgr -np show association where account=$1 | cut -f3 -d\| | awk 'NF'
+}
+
+get_gpu_usage_raw() {
+    scontrol -o show assoc_mgr | grep "Account=$1 UserName=$2" | awk '{print $8}' | awk -F/ '{print $3}' | sed -e 's/Efctv=//g'
+}
+
+used_raw_sum=0
+used_hour_sum=0
+
+for project in ${projects[@]};
+do
+    for user in `get_users $project`;
+    do
+        gpu_usage_raw=`get_gpu_usage_raw $project $user`;
+        gpu_usage_hour=`echo $gpu_usage_raw/57600 | bc`;
+
+        echo "gpu,project=$project,user=$user used_raw=$gpu_usage_raw,used_hour=$gpu_usage_hour `date +%s%N`"
+        used_raw_sum=`echo $used_raw_sum + $gpu_usage_raw | bc`
+        used_hour_sum=`echo $used_hour_sum + $gpu_usage_hour | bc`
+    done
+done
+
+echo $used_hour_sum
+echo $used_raw_sum
--- telegraf/get_devana_data.sh
+++ telegraf/get_devana_data.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-projects=(p904-24-3)
-
-get_users() {
-	ssh devana "sacctmgr -np show association where account=$1 | cut -f3 -d\| | awk 'NF'"
-}
-
-for project in ${projects[@]};
-do
-    for user in `get_users $project`;
-    do
-        echo "checking: $user";
-    done
-done
+ssh devana "/home/rbelanec/get_data.sh"
